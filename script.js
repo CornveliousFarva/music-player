88e0ce606266dec501e7a8a216cf7f10d3d68c5a
Greg Schuman <gcschuman@hotmail.com>
2020-09-13 14:24:05 -0700

    Added script for previous song.

--- script.js
+++ script.js
@@ -43,3 +43,14 @@ function pauseSong(){
 }
 
 //Previous Song
+function prevSong(){
+    songIndex--;
+    if (songIndex < 0){
+        songIndex = songs.length -1;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
